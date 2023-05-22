a9dd3bee054da8ae542c9fe28e65f28b0c58c3b2
nika kobaidze <nika.kobaidze@gmail.com>
2023-05-23 01:28:29 +0400

    [ADD] list render

--- my_app/src/components/Check.jsx
+++ my_app/src/components/Check.jsx
@@ -7,66 +7,29 @@ class Check extends Component {
         super(props);
 
         this.state = {
-            men: [
-                { name: 'nika', age: 34 },
-            ],
-            username: '',
-            phonenumber: '',
-            email: '',
-            usernameError: ''
+            age: ''
         }
     }
 
 
-    changeData = (e) => {
-        let val = e.target.value;
-        let name = e.target.name;
 
-        this.setState({
-            [name]: val
-        })
-    }
 
-    formSubmit = (e) => {
-        e.preventDefault();
-        console.log("FORM SUBMIT");
-        console.log(this.state);
-        if (this.validate()) {
-            console.log("VALIDATION DONE");
-        }
-    }
 
-    validate() {
-        if (!this.state.username) {
-            this.setState({ usernameError: "USERNAME IS EMPTY" })
-        } else {
-            this.setState({ usernameError: "" })
-            return true;
-        }
-    }
 
     render() {
 
+        const names = ['alex', 'nika', 'kobi'];
 
+        const items = names.map((item, index) => {
+            return <li key={index}>{item}</li>
+        })
 
         return (<div className='app'>
-            <h1>Hello nikoo</h1>
+            <h1>Student list</h1>
             <div>
-                <form onSubmit={this.formSubmit}>
-                    <div className="form-group" style={{ marginTop: "200px" }}>
-                        <label>User Name:</label>
-                        <input type='text' name="username" onChange={(e) => { this.setState({ username: e.target.value }) }}></input>
-                        <strong style={{ color: 'red' }}>{this.state.usernameError}</strong>
-                        <br></br>
-                        <label>Phone number:</label>
-                        <input type='text' name="phonenumber" onChange={(e) => { this.setState({ phonenumber: e.target.value }) }}></input>
-                        <br></br>
-                        <label>Email:</label>
-                        <input type='text' name="email" onChange={(e) => { this.setState({ email: e.target.value }) }}></input>
-                        <br></br>
-                        <button type='submit'>Submit</button>
-                    </div>
-                </form>
+                <ul>
+                    {items}
+                </ul>
             </div>
         </div>)
     }
