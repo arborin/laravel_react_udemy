0a5537975805b4dfacb8b327c68e87ee735f7f92
nika kobaidze <nika.kobaidze@gmail.com>
2023-05-25 22:22:52 +0400

    [MERGE]

--- my_app/src/App.js
+++ my_app/src/App.js
@@ -1,12 +1,10 @@
-// import logo from './logo.svg';
 import './App.css';
-import Check from './components/Check';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Check />
+
       </header>
     </div>
   );
--- my_app/src/components/Hello.jsx
+++
@@ -1,5 +0,0 @@
-function Hello(props) {
-    return <h5>HELLO WORLD {props.name} - {props.price}</h5>
-}
-
-export default Hello;
