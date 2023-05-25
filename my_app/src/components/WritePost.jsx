fa60631318d6b414549d75ce5dc041df21614d1c
nkobaidze <nika.kobaidze@gmail.com>
2023-05-25 18:01:00 +0400

    [ADD] form

--- my_app/src/components/WritePost.jsx
+++ my_app/src/components/WritePost.jsx
@@ -2,17 +2,29 @@ import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 
 export default class WritePost extends Component {
+
+    state = {
+        name: '',
+        email: ''
+    }
+
+    formSubmit = (e) => {
+        e.preventDefault();
+    }
+
     render() {
         return (
             <div>
                 <Link to='/'>Back</Link>
                 <h1>Write Post</h1>
-                <form>
-                    <label>Name:</label>
-                    <input type='text' />
+                <form onSubmit={this.formSubmit}>
+                    <label>Name: {this.state.name}</label>
+                    <input type='text' onChange={(e) => { this.setState({ name: e.target.value }) }} />
                     <br />
-                    <label>Email:</label>
-                    <input type='text' />
+                    <label>Email: {this.state.email}</label>
+                    <input type='text' onChange={(e) => { this.setState({ email: e.target.value }) }} />
+                    <br />
+                    <button type='submit'>Send</button>
                 </form>
             </div>
         )
