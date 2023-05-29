403eeeb3074893bfbc9cb9965174bcb1cc28bfe2
nika kobaidze <nika.kobaidze@gmail.com>
2023-05-29 23:35:19 +0400

    [ADD] new routes

--- basic_app/routes/web.php
+++ basic_app/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ContactController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/about', function () {
+    return view("about");
+});
+
+Route::get('/contact', [ContactController::class, 'index']);
