ff78870f6f24d1ca700c1675eaa18177f8b1bee6
nkobaidze <nika.kobaidze@gmail.com>
2023-07-10 17:52:10 +0400

    [ADD] banner module 19

--- new_site/src/components/TopBanner/TopBanner.jsx
+++ new_site/src/components/TopBanner/TopBanner.jsx
@@ -1,16 +1,23 @@
 import React from 'react'
 import '../../asset/css/custom.css'
-import Container from 'react-bootstrap/Container'
-import Row from 'react-bootstrap/Row'
+import { Container, Row, Col, Button } from 'react-bootstrap'
 
 
 export default function TopBanner() {
     return (
         <>
-            <Container className='topFixedBanner'>
-                <Row>
-
-                </Row>
+            <Container fluid={true} className='topFixedBanner p-0'>
+                <div className='topBannerOverlay'>
+                    <Container className='topContent'>
+                        <Row>
+                            <Col className='text-center'>
+                                <h1 className='topTitle'>React Learning </h1>
+                                <h4 className='topSubTitle'>Learn step by step</h4>
+                                <Button variant='primary'>Show More</Button>
+                            </Col>
+                        </Row>
+                    </Container>
+                </div>
             </Container>
         </>
     )
