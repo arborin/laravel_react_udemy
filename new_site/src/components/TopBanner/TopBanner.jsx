9c2eed4c60da986a5d0e756f3cac47ec6d811747
nkobaidze <nika.kobaidze@gmail.com>
2023-07-10 13:50:01 +0400

    [ADD] banner segment

---
+++ new_site/src/components/TopBanner/TopBanner.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import '../../asset/css/custom.css'
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+
+
+export default function TopBanner() {
+    return (
+        <>
+            <Container className='topFixedBanner'>
+                <Row>
+
+                </Row>
+            </Container>
+        </>
+    )
+}
