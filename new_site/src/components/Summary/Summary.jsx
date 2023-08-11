447807f3f7a97617be54f03c65eeb97ed86bf46d
nkobaidze <nika.kobaidze@gmail.com>
2023-08-11 18:02:20 +0400

    [ADD

--- new_site/src/components/Summary/Summary.jsx
+++ new_site/src/components/Summary/Summary.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEnvelope, faCheckSquare } from '@fortawesome/free-solid-svg-icons'
 import { Col, Container, Row, Card } from 'react-bootstrap'
+import CountUp from 'react-countup';
 
 export default function Summary() {
     return (
@@ -9,12 +12,22 @@ export default function Summary() {
                     <Col lg={8}>
                         <Row>
                             <Col>
-                                <h1>25000</h1>
+                                <h1>
+
+                                    <CountUp start={0} end={100}>
+                                        {({ countUpRef, start }) => (
+                                            <div>
+                                                <span ref={countUpRef} />
+                                                <button onClick={start}>Start</button>
+                                            </div>
+                                        )}
+                                    </CountUp>
+                                </h1>
                                 <h4 >Students worldwide</h4>
                                 <hr />
                             </Col>
                             <Col>
-                                <h1> 25000</h1>
+                                <h1><FontAwesomeIcon icon={faEnvelope} /> 25000</h1>
                                 <h4>Students worldwide</h4>
                                 <hr className='w-25' />
                             </Col>
@@ -29,18 +42,17 @@ export default function Summary() {
                         <Row>
                             <Card>
                                 <Card.Body>
-                                    <Card.Title>title</Card.Title>
-                                    <Card.Text>tesxt</Card.Text>
-                                    <Card.Text>tesxt</Card.Text>
-                                    <Card.Text>tesxt</Card.Text>
-                                    <Card.Text>tesxt</Card.Text>
+                                    <Card.Text><FontAwesomeIcon className={"text-success"} icon={faCheckSquare}></FontAwesomeIcon>title</Card.Text>
+                                    <Card.Text><FontAwesomeIcon icon={faCheckSquare}></FontAwesomeIcon>title</Card.Text>
+                                    <Card.Text><FontAwesomeIcon icon={faCheckSquare}></FontAwesomeIcon>title</Card.Text>
+                                    <Card.Text><FontAwesomeIcon icon={faCheckSquare}></FontAwesomeIcon>title</Card.Text>
+                                    <Card.Text><FontAwesomeIcon icon={faCheckSquare}></FontAwesomeIcon>title</Card.Text>
                                 </Card.Body>
-
                             </Card>
                         </Row>
                     </Col>
                 </Row >
-            </Container >
+            </Container>
         </>
     )
 }
